9b5d7817b954b5fd72c8d65bd7d8550cbaf26484
Alexander Momchilov <alexandermomchilov@gmail.com>
2021-11-28 14:56:41 -0500

    Add XPCConnectionType enum

---
+++ Sources/SecureXPC/XPCConnectionType.swift
@@ -0,0 +1,13 @@
+//
+//  XPCConnectionType.swift
+//  
+//
+//  Created by Alexander Momchilov on 2021-11-28.
+//
+
+import Foundation
+
+public enum XPCConnectionType {
+    case xpcServiceClient
+    case machServiceClient
+}
