f8045638c98b022564642982ac4c1d987b93bcc4
Alexander Momchilov <alexandermomchilov@gmail.com>
2021-12-15 21:46:07 -0500

    Refactor `Request` initializers

--- Sources/SecureXPC/Request.swift
+++ Sources/SecureXPC/Request.swift
@@ -11,7 +11,6 @@ import Foundation
 ///
 /// A request always contains a route and optionally contains a payload.
 struct Request {
-    
     private enum RequestKeys {
         static let route: XPCDictionaryKey = const("__route")
         static let payload: XPCDictionaryKey = const("__payload")
@@ -25,37 +24,46 @@ struct Request {
     ///
     /// If  `containsPayload` is `true` then `decodePayload` can be called to decode it; otherwise calling this function will result an error being thrown.
     let dictionary: xpc_object_t
-    
+}
+
+extension Request {
     /// Represents a request that's already been encoded into an XPC dictionary.
     ///
     /// This initializer is expected to be used by the server when receiving a request which it now needs to decode.
     init(dictionary: xpc_object_t) throws {
-        self.dictionary = dictionary
-        self.route = try XPCDecoder.decode(XPCRoute.self, from: dictionary, forKey: RequestKeys.route)
-        self.containsPayload = try XPCDecoder.containsKey(RequestKeys.payload, inDictionary: self.dictionary)
+        self.init(
+            route: try XPCDecoder.decode(XPCRoute.self, from: dictionary, forKey: RequestKeys.route),
+            containsPayload: try XPCDecoder.containsKey(RequestKeys.payload, inDictionary: dictionary),
+            dictionary: dictionary
+        )
     }
-    
+
+    private init(route: XPCRoute, encodedPayload: xpc_object_t?) throws {
+        let dictionary = xpc_dictionary_create(nil, nil, 0)
+        xpc_dictionary_set_value(dictionary, RequestKeys.route, try XPCEncoder.encode(route))
+        if let encodedPayload = encodedPayload {
+            xpc_dictionary_set_value(dictionary, RequestKeys.payload, encodedPayload)
+        }
+
+        self.init(
+            route: route,
+            containsPayload: encodedPayload != nil,
+            dictionary: dictionary
+        )
+    }
+
     /// Represents a request without a payload which has yet to be encoded into an XPC dictionary.
     ///
     /// This initializer is expected to be used by the client in order to send a request across the XPC connection.
     init(route: XPCRoute) throws {
-        self.route = route
-        self.containsPayload = false
-        
-        self.dictionary = xpc_dictionary_create(nil, nil, 0)
-        xpc_dictionary_set_value(self.dictionary, RequestKeys.route, try XPCEncoder.encode(route))
+        try self.init(route: route, encodedPayload: nil)
     }
     
     /// Represents a request with a payload which has yet to be encoded into an XPC dictionary.
     ///
     /// This initializer is expected to be used by the client in order to send a request across the XPC connection.
     init<P: Encodable>(route: XPCRoute, payload: P) throws {
-        self.route = route
-        self.containsPayload = true
-        
-        self.dictionary = xpc_dictionary_create(nil, nil, 0)
-        xpc_dictionary_set_value(self.dictionary, RequestKeys.route, try XPCEncoder.encode(self.route))
-        xpc_dictionary_set_value(dictionary, RequestKeys.payload, try XPCEncoder.encode(payload))
+        try self.init(route: route, encodedPayload: try XPCEncoder.encode(payload))
     }
     
     /// Decodes the payload as the provided type.
